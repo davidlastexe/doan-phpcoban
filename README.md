91eefbcac2adcc0e4e273b100e48cf4929ac3387
davidngoc321 <vpsgithub.1@outlook.com>
2025-09-23 08:59:58 +0000

    feat: update .gitignore, README, and add PageController with routing

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # doan-phpcoban
+
+## How to run this project
+
+1. Clone my project then run `npm i` and `composer install`.
+2. Then install xampp and start it.
--- public/index.php
+++ public/index.php
@@ -9,20 +9,20 @@ require_once __DIR__.'/../app/Configs/configs.php';
 use App\Core\Router;
 use App\Controllers\HomeController;
 use App\Controllers\AuthController;
-// use App\Controllers\PageController;
-// use App\Controllers\Api\AuthController as ApiAuthController;
+use App\Controllers\PageController;
+use App\Controllers\Api\AuthController as ApiAuthController;
 
 require_once _PATH_URL_APP.'/Helpers/helpers.php';
 
 $router = new Router();
 
 $router->get('/', HomeController::class, 'index');
-// $router->get('/dashboard', PageController::class, 'dashboard');
+$router->get('/dashboard', PageController::class, 'dashboard');
 $router->get('/login', AuthController::class, 'showLoginForm');
 $router->get('/register', AuthController::class, 'showRegisterForm');
-// $router->get('/api/check-email', ApiAuthController::class, 'checkEmail');
+$router->get('/api/check-email', ApiAuthController::class, 'checkEmail');
 
-// $router->post('/register', AuthController::class, 'handleRegister');
+$router->post('/register', AuthController::class, 'handleRegister');
 
 $requestUri = $_SERVER['REQUEST_URI'];
 $requestPath = parse_url($requestUri, PHP_URL_PATH);
